08090fa164793db6493338c646ea0cd66045f0d4
Mukund Raghav Sharma (Moko) <mukund.sharma92@gmail.com>
2023-01-02 10:36:44 -0800

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+import { ChartBarIcon } from "@heroicons/react/outline"
+
+function Header() {
+  return (
+    <div className="flex items-center shadow-md justify-between">
+        {/* Left */}
+        <div className="flex items-center">
+          <ChartBarIcon className="h-4 ml-4"/>
+          <h1 className="ml-2 font-bold"> Inflation Charts </h1>
+        </div>
+
+        {/* Right */}
+        <div className="mx-3">
+
+          <Link href="https://twitter.com/MokoSharma">
+            <Image className="rounded-full m-2" 
+                    src="https://cdn-icons-png.flaticon.com/512/124/124021.png"
+                    alt=""
+                    width={40}
+                    height={40} />
+          </Link>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
